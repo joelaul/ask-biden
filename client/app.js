828b0de234c292c2ce6d0c98873068d5d682ae4e
Joe Lauletta <laulettaj@gmail.com>
2023-04-07 15:37:47 -0400

    make modularity improvements to codebase

--- client/app.js
+++ client/app.js
@@ -1,12 +1,5 @@
-/* TODO
-
-FX - LOAD STATIC, CONFETTI, FALLING STARS
-
-SEND FULL CONVERSATION TO API
-
-*/
-
 // DOM
+
 const form = document.querySelector('form');
 const textArea = document.querySelector('textarea');
 const voice = document.querySelectorAll('form button')[0];
@@ -23,7 +16,7 @@ let chatContainer;
 const usedPrompts = {};
 const userBubbleIDs = [], joeBubbleDivs = [];
 
-let audio, analyser, dataArray, formText;
+let formText, audio, analyser, dataArray;
 let throttleTimer, listenTimer;
 let listening, sending = false;
 
@@ -40,14 +33,6 @@ const sendSound = new Audio(sendSoundUrl);
 
 // FUNCTIONS
 
-const makeUniqueID = () => {
-    const timeStamp = Date.now();
-    const randomNumber = Math.random();
-    const hex = randomNumber.toString(16);
-
-    return `id-${timeStamp}-${hex}`;
-}
-
 const initAudio = () => {
     audio = new Audio();
     audio.addEventListener('ended', () => {
@@ -63,20 +48,46 @@ const initAudio = () => {
 
     audioNode.connect(analyser);
     analyser.connect(audioCtx.destination);
-};
+}
+
+const startListening = () => {
+    stt.start();
+    listening = true;
+    middleSlide.classList.add('slide-out');
+    setTimeout(() => {
+        listen.classList.remove('hide');
+    }, 100);
+}
+
+const stopListening = () => {
+    stt.stop();
+    listen.classList.add('hide')
+    clearTimeout(listenTimer);
+    listening = false;
+}
+
+const makeUniqueID = () => {
+    const timeStamp = Date.now();
+    const randomNumber = Math.random();
+    const hex = randomNumber.toString(16);
+
+    return `id-${timeStamp}-${hex}`;
+}
 
 const draw = () => {
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
     requestAnimationFrame(draw);
+    dataArray = new Uint8Array(analyser.fftSize); 
     analyser.getByteTimeDomainData(dataArray); 
 
     const delta = (WIDTH * 1.0) / analyser.fftSize;
     let x = 0;
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
     canvasCtx.beginPath();
 
     for (let i = 0; i < analyser.fftSize; i++) {
-        let v = dataArray[i] / 128.0; // -128 < dataArray[i] < 128; -1 < v < 1
-        let y = (v * HEIGHT) / 2; // -HEIGHT/2 < y < HEIGHT/2
+        let v = dataArray[i] / 128.0;
+        let y = (v * HEIGHT) / 2;
 
         if (i === 0) {
             canvasCtx.moveTo(x, y);
@@ -89,9 +100,51 @@ const draw = () => {
 
     canvasCtx.lineTo(WIDTH, HEIGHT / 2);
     canvasCtx.stroke();
-};
+}
 
-const armJoeBubbles = () => {
+const renderUserBubble = (prompt) => {
+    const userBubbleDiv = document.createElement('div');
+    userBubbleDiv.classList.add('chat-bubble', 'user');
+    userBubbleDiv.innerText = prompt;
+
+    userBubbleDiv.id = makeUniqueID();
+    userBubbleIDs.push(userBubbleDiv.id);
+
+    chatContainer.append(userBubbleDiv);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+const renderJoeResponse = async (response, prompt) => {
+    const data = await response.json();
+    const text = data.text;
+    const audioBuffer = new Uint8Array(data.audio.data);
+    const blob = new Blob([audioBuffer], { type: 'audio/mpeg' });
+    const url = URL.createObjectURL(blob);
+    audio.src = url;
+
+    const joeBubbleDiv = document.createElement('div');
+    joeBubbleDiv.classList.add('chat-bubble', 'joe');
+    joeBubbleDiv.innerText = text;
+
+    joeBubbleDivs.push(joeBubbleDiv);
+    chatContainer.append(joeBubbleDiv);
+
+    loader.classList.add('hide');
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // cache response for re-use
+    usedPrompts[prompt] = [url, text];
+}
+
+const playJoeResponse = () => {
+    setTimeout(() => {
+        canvas.classList.remove('hide')
+    }, 100);
+    audio.play();
+    draw();
+}
+
+const repopulateJoeBubbles = () => {
     for (let i = 0; i < joeBubbleDivs.length; i++) {
         joeBubbleDivs[i].addEventListener('click', () => {
             handleJoeBubble(userBubbleIDs[i]);
@@ -99,7 +152,9 @@ const armJoeBubbles = () => {
     }
 }
 
-const handleJoeBubble = (id) => {    
+// HANDLERS
+
+const handleJoeBubble = (id) => {
     const itsPrompt = document.getElementById(id).innerText;
     const itsAudio = usedPrompts[itsPrompt][0];
     audio.src = itsAudio;
@@ -107,20 +162,12 @@ const handleJoeBubble = (id) => {
     middleSlide.classList.add('slide-out');
     dataArray = new Uint8Array(analyser.fftSize); 
 
-    audio.play();
-    canvas.classList.remove('hide');
-    draw();
+    playJoeResponse();
 }
 
 const handleVoice = () => {
-    if (!listening && !sending && audio.ended) {
-
-        stt.start();
-        listening = true;
-        middleSlide.classList.add('slide-out');
-        setTimeout(() => {
-            listen.classList.remove('hide');
-        }, 100);
+    if (!listening && !sending && (!audio || audio.ended)) {
+        startListening();
 
         // stop listening if 3 seconds pass without audio
         if (listenTimer) {
@@ -128,12 +175,7 @@ const handleVoice = () => {
         } else {
             listenTimer = setTimeout(() => {
                 middleSlide.classList.remove('slide-out');
-                stt.stop();
-                setTimeout(() => {
-                    listen.classList.add('hide')
-                }, 100);
-                listening = false; // reusable function?
-                listenTimer = null;
+                stopListening();
             }, 3000);
         }
 
@@ -153,22 +195,16 @@ const handleAsk = async (prompt) => {
         middleSlide.classList.add('slide-out');
         textArea.value = '';
 
-        // offset middleContainer on first chatContainer injection
         if (!chatContainer) {
             chatContainer = document.createElement('div');
             chatContainer.classList.add('chat-container');
             contentWrapper.append(chatContainer);
         }
         
-        // check - was text sent during listening? if so, stop listening + clear listenTimer
         if (listening) {
-            stt.stop();
-            listen.classList.add('hide')
-            clearTimeout(listenTimer);
-            listening = false;
+            stopListening();
         }
 
-        // check - has audio been initialized?
         if (audio) {
             if (!audio.ended) {
                 audio.pause();
@@ -178,7 +214,7 @@ const handleAsk = async (prompt) => {
             initAudio();
         }
 
-        // check - has it been asked? (Y = use cache, N = throttle/fetch)
+        // has it been asked? (Y = use cache, N = throttle/fetch)
         if (usedPrompts[prompt]) {
             audio.src = usedPrompts[prompt][0];
         } else {
@@ -186,28 +222,20 @@ const handleAsk = async (prompt) => {
             if (throttleTimer) {
                 return;
             } else {
-                // show loader, render user chat bubble
+                throttleTimer = setTimeout(() => {
+                    clearTimeout(throttleTimer);
+                    throttleTimer = null;
+                    }, 5000);
+
+                renderUserBubble(prompt);
                 sendSound.play();
 
+                // show loader
                 setTimeout(() => {
                     loader.classList.remove('hide')
                 }, 100);
-                throttleTimer = setTimeout(() => {
-                clearTimeout(throttleTimer);
-                throttleTimer = null;
-                }, 5000); 
-
-                let userBubbleDiv = document.createElement('div');
-                userBubbleDiv.classList.add('chat-bubble', 'user');
-                userBubbleDiv.innerText = prompt;
-
-                userBubbleDiv.id = makeUniqueID();
-                userBubbleIDs.push(userBubbleDiv.id);
-
-                chatContainer.append(userBubbleDiv);
-                chatContainer.scrollTop = chatContainer.scrollHeight;
-
-                // get biden response
+                
+                // get joe response
                 const response = await fetch('http://localhost:8000', 
                 {
                     method: 'POST',
@@ -216,44 +244,14 @@ const handleAsk = async (prompt) => {
                     },
                     body: JSON.stringify({ prompt: prompt })
                 });
-
-                // render biden response
-                if (response.ok) {
-                    const data = await response.json();
-                    const text = data.text;
-                    const audioBuffer = new Uint8Array(data.audio.data);
-                    const blob = new Blob([audioBuffer], { type: 'audio/mpeg' });
-                    const url = URL.createObjectURL(blob);
-                    audio.src = url;
-
-                    let joeBubbleDiv = document.createElement('div');
-                    joeBubbleDiv.classList.add('chat-bubble', 'joe');
-                    joeBubbleDiv.innerText = text;
-
-                    joeBubbleDivs.push(joeBubbleDiv);
-                    chatContainer.append(joeBubbleDiv);
-
-                    // hide loader, refresh chat log
-                    loader.classList.add('hide');
-                    chatContainer.scrollTop = chatContainer.scrollHeight;
-
-                    // cache response for re-use
-                    usedPrompts[prompt] = [url, text];
-                }
+                if (response.ok) await renderJoeResponse(response, prompt);
             }
         }
 
-        setTimeout(() => {
-            canvas.classList.remove('hide')
-        }, 100);
-        dataArray = new Uint8Array(analyser.fftSize); 
-        
-        audio.play();
-        draw();
-
-        armJoeBubbles();
+        playJoeResponse();
+        repopulateJoeBubbles();
         sending = false;
-    }   
+    }
 }
 
 // INIT
